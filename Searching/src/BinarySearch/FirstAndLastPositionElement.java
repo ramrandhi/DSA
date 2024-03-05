90174b27830c3443e25a95e9d6bae4478c8da101
RAMANJANEYULU <ramanjaneyulu.randhi@healthcarenow.in>
2024-03-05 09:24:49 +0530

    first nd Last Postion of a number using bs

--- Searching/src/BinarySearch/FirstAndLastPositionElement.java
+++ Searching/src/BinarySearch/FirstAndLastPositionElement.java
@@ -11,11 +11,31 @@ public class FirstAndLastPositionElement {
 		System.out.println(Arrays.toString(indexes));
 	}
 	static int[] firstAndLastPositionElement(int[] arr, int target) {
+		
+		int start = numberFrequency(arr, target, true);
+		int end = numberFrequency(arr, target, false);
+		int[] a = {start,end};
+		return a;
+	}
+	private static int numberFrequency(int[] arr, int target, boolean isFirstElement) {
+		int ans = -1;
 		int start = 0;
 		int end = arr.length-1;
 		while(start <= end) {
 			int mid = start + (end-start)/2;
+			if(target < arr[mid]) {
+				end = mid-1;
+			} else if(target > arr[mid]) {
+				start = mid + 1;
+			}else {
+				ans = mid;
+				if(isFirstElement) {
+					end = mid-1;
+				}else {
+					start = mid+1;
+				}
+			}
 		}
-		return new int[] {-1,-1};
+		return ans;
 	}
 }
