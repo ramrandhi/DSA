4619aec5c5f820eb0f2f883dc7745189f200ce2f
RAMANJANEYULU <ramanjaneyulu.randhi@healthcarenow.in>
2024-03-19 22:26:26 +0530

    new project initiated for sorting

---
+++ Searching/src/BinarySearch/SearchInRotatedSortedArray.java
@@ -0,0 +1,17 @@
+package BinarySearch;
+
+public class SearchInRotatedSortedArray {
+
+	public static void main(String[] args) {
+		int[] arr = {3,4,5,6,7,2,1,0,-5};
+		int target = 6;
+		int index = searchInSortedArray(arr, target);
+		System.out.println(index);
+
+	}
+	
+	static int searchInSortedArray(int[] arr, int target){
+		return -1;
+	}
+
+}
